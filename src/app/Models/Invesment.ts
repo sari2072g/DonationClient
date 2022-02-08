666f7741b5baa198aa22188e9488f7ce88fe5d47
sarigutman <sarig2072@gmail.com>
2022-02-08 18:37:06 +0200

    commit

---
+++ src/app/Models/Invesment.ts
@@ -0,0 +1,12 @@
+export class Invesment {
+time:Date;
+price_difference:number
+    open: number;
+    close:number
+    high: number = null;
+    low: number = null;
+    adjusted_close: number = null;
+    volume: number = null;
+    dividend_amount: number = null;
+    split_coefficient?: number = null;
+}
--- src/app/components/investment/investment.component.ts
+++ src/app/components/investment/investment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Invesment } from 'src/app/Models/Invesment';
 import { InvestmentService } from 'src/app/services/investment.service';
 
 @Component({
@@ -7,35 +8,88 @@ import { InvestmentService } from 'src/app/services/investment.service';
   styleUrls: ['./investment.component.css']
 })
 export class InvestmentComponent implements OnInit {
-  listOfInvestment:any[]=[]
-first:number=0
-totalRecords=20;
-  constructor(private investmentService:InvestmentService) { }
+  listOfInvestment: any[] = []
+  first: number = 0
+  totalRecords = 20;
+  cols: any[] = [];
+  rows = 10;
+  Max: number;
+  dateSale: any;
+  buyDate: any;
+  min: any;
+  Min: any;
+  index: any;
+
+  constructor(private investmentService: InvestmentService) { }
   ngOnInit(): void {
     debugger
     console.log(this.listOfInvestment);
-    
-    this.investmentService.getTimeSesiedList().subscribe(getdata=>{
+
+    this.investmentService.getTimeSesiedList().subscribe(getdata => {
       debugger
       var result = [];
       var getTimeList = getdata['Time Series (Daily)'];
-   
-for(var i in getTimeList)
-    result.push([i, getTimeList [i]]);  
-     const mone=0
-    result.forEach(element => {
-      debugger;
-  //     const resultp=[];
-  //     for(var i in element[1]){}
-  //  const a =   Object.assign(element[1][i.replace(/[0-9]\./g, '')])
-//     resultp.push(element[1][i.replace(/[0-9]\./g, '')]);
-    
-      const elementList = Object.assign({time:element[0], detailsDay:element[1]
-     
-     })
-  
-     this.listOfInvestment.push(elementList);
-    });
+
+      for (var i in getTimeList)
+        result.push([i, getTimeList[i]]);
+      const mone = 0
+      result.forEach(element => {
+        const object = new Invesment();
+        object.open = element[1]['1. open']
+        object.time = element[0];
+        object.close = element[1]['4. close']
+        object.high = element[1]['2. high']
+        object.low = element[1]['3. low']
+        object.adjusted_close = element[1]['5. adjusted close']
+        object.volume = element[1]['6. volume']
+        object.dividend_amount = element[1]['7. dividend amount']
+        object.price_difference = object.close - object.open;
+        this.listOfInvestment.push(object);
+      });
+
+      this.cols = [
+        { field: 'time', header: 'תאריך' },
+        { field: 'open', header: 'מחיר פתיחה' },
+        { field: 'high', header: 'מחיר הכי גבוה' },
+        { field: 'low', header: 'מחיר הכי נמוך' },
+        { field: 'close', header: 'מחיר סגירה' },
+
+
+        { field: 'price_difference', header: 'הפרש מחירים' }
+      ];
+      // במידה והמערך לא ממוין נעשה לו מיון לפני שנחשב
+      this.listOfInvestment = this.listOfInvestment.sort((a, b) => {
+        return <any>new Date(a.time) - <any>new Date(b.time);
+      });
+      let max = 0;
+      let indexMax, indexMin;
+      debugger
+      //בהנחה שהמערך ממוין
+      for (let i = 0; i < this.listOfInvestment.length; i++) {
+        for (let j = this.listOfInvestment.length - 1; j > i; j--)
+          if (Number((this.listOfInvestment[j].close)) - Number(this.listOfInvestment[i].open) > max) {
+            max = Number((this.listOfInvestment[j].close)) - Number(this.listOfInvestment[i].open);
+            indexMax = j;
+            indexMin = i;
+          }
+      };
+      this.Max = this.listOfInvestment[indexMax].close;
+      this.dateSale = this.listOfInvestment[indexMax].time;
+      this.buyDate = this.listOfInvestment[indexMin].time
+      this.Min = this.listOfInvestment[indexMin].open
     })
+  }
+
+
+
+  next() {
+    this.first = this.first + this.rows;
+  }
+
+  prev() {
+    this.first = this.first - this.rows;
+  }
+  isLastPage(): boolean {
+    return this.listOfInvestment ? this.first === (this.listOfInvestment.length - this.rows) : true;
   }
 }
